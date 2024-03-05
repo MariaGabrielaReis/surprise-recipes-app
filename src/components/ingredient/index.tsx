da6b9bc84689a7e00a630eb19c39b93fa68b9d3f
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-03-05 09:25:26 -0300

    feat: create ingredient component

---
+++ src/components/ingredient/index.tsx
@@ -0,0 +1,31 @@
+import { Image, Pressable, PressableProps, Text } from "react-native";
+
+import { theme } from "@/theme";
+import { styles } from "./styles";
+
+export type IngredientProps = PressableProps & {
+  name: string;
+  image: string;
+  selected?: boolean;
+};
+
+export default function Ingredient({
+  name,
+  image,
+  selected = false,
+  ...rest
+}: IngredientProps) {
+  return (
+    <Pressable
+      style={[styles.container, selected && styles.selected]}
+      {...rest}
+    >
+      <Image style={styles.image} />
+      <Text
+        style={[styles.title, selected && { color: theme.colors.green_600 }]}
+      >
+        {name}
+      </Text>
+    </Pressable>
+  );
+}
