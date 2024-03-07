87018477b869e1509d6961b491e5cb9f148e6ec7
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-03-07 12:46:28 -0300

    feat: create services for recipes, preparations and ingredients

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,12 @@
 import * as ingredients from "./ingredients";
+import * as preparations from "./preparations";
+import * as recipes from "./recipes";
 
 export const services = {
+  recipes,
   ingredients,
+  preparations,
+
   storage: {
     imagePath: process.env.EXPO_PUBLIC_SUPABASE_STORAGE_URL ?? "",
   },
